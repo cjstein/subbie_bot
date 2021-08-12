25bfd90705c4427c3e183e7762ae20ab1e729b18
cjstein <cjstein9@gmail.com>
2021-08-12 12:40:38 -0500

    initial commit

---
+++ insult.py
@@ -0,0 +1,51 @@
+import requests
+import random
+import time
+
+INSULT_FILE = r'/home/SubZeb/twitchbot/insults.txt'
+
+
+def check_insult_duplicate(insult: str):
+    with open(INSULT_FILE, 'r') as f:
+        insults = f.readlines()
+    insults = [i.strip().lower() for i in insults]
+    return insult.strip().lower() in insults
+
+
+def get_insult_from_api():
+    # This would get the insult from the insult generator
+    url = 'https://evilinsult.com/generate_insult.php?lang=en&type=text'
+    r = requests.get(url)
+    return r.text
+
+
+def get_insult():
+    # This funxction gets a random insult from the compiled insults from the generator
+    with open(INSULT_FILE, 'r') as f:
+        insults = f.readlines()
+    return random.choice(insults)
+
+
+def add_insult(insult):
+    # This function takes the insult and adds it to the list
+    if not check_insult_duplicate(insult):
+        with open(INSULT_FILE, 'a') as f:
+            f.write(insult.strip())
+            f.write('\n')
+
+
+def main():
+    insults = []
+    i = 0
+    while i < 2000:
+        insult = get_insult()
+        print(str(i) + ": " + insult)
+        if insult not in insults:
+            insults.append(insult)
+            add_insult(insult)
+        i += 1
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,58 @@
+#!/home/SubZeb/.virtualenvs/twitchbot/bin/python3
+import config
+import utility
+import socket
+import time
+import re
+import insult
+
+CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+try:
+    s = socket.socket()
+    s.connect((config.HOST, config.PORT))
+    s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
+    s.send("NICK {}\r\n".format(config.NICK).encode("utf-8"))
+    s.send("JOIN {}\r\n".format(config.CHAN).encode("utf-8"))
+    connected = True  # Socket successfully connected
+    print("Connected...")
+except Exception as e:
+    print(str(e))
+    connected = False  # Socket failed to connect
+
+
+def bot_loop():
+    while connected:
+        response = s.recv(1024).decode("utf-8")
+        if response == "PING :tmi.twitch.tv\r\n":
+            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            print("Pong")
+        else:
+            username = re.search(r"\w+", response).group(0)
+            message = CHAT_MSG.sub("", response)
+            print(username)
+            print(message, "\n")
+            for pattern in config.BAN_PAT:
+                if re.match(pattern, message):
+                    utility.ban(s, username)
+                    break
+            if message.strip().lower() == "!vibe" and username.lower() == "subzeb":
+                utility.chat(s, "Subbie is always vibing at 100%")
+            if message.strip().lower() == "!insult":
+                chat_out = insult.get_insult()
+                print(chat_out)
+                utility.chat(s, chat_out)
+            if message.strip().lower().startswith("!addinsult"):
+                try:
+                    addition = message[11:]
+                    insult.add_insult(addition)
+                    utility.chat(s, 'Insult added')
+                    print("Added insult: " + addition)
+                except Exception as e:
+                    utility.chat(s, 'There was an error adding the insult')
+                    print(e)
+    time.sleep(1 / config.RATE)
+
+
+if __name__ == "__main__":
+    bot_loop()
