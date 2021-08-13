15c06fc358f441bc062c7f2b4378b7c03833274a
cjstein <cjstein9@gmail.com>
2021-08-13 09:58:28 -0500

    added config example file

--- config_example.py
+++ config_example.py
@@ -6,4 +6,4 @@ CHAN = "#taw_tv"
 RATE = (20/30)
 BAN_PAT = [
 ]
-INSULT_FILE = r'/home/SubZeb/twitchbot/insults.txt' # you will need to create an insult.txt file with each insult on a new line
+INSULT_FILE = r'absolute location of insults text file' # you will need to create an insult.txt file with each insult on a new line
