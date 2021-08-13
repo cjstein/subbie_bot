6bf2437c56cc7d23db6504caddf27d2387e18679
cjstein <cjstein9@gmail.com>
2021-08-13 10:01:10 -0500

    removed used channel

--- config_example.py
+++ config_example.py
@@ -2,8 +2,8 @@ HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "subbie_bot"
 PASS = "oauth:place oauth code here"
-CHAN = "#taw_tv"
+CHAN = "#channel_name"  # All lowercase
 RATE = (20/30)
 BAN_PAT = [
 ]
-INSULT_FILE = r'absolute location of insults text file' # you will need to create an insult.txt file with each insult on a new line
+INSULT_FILE = r'absolute location of insults text file'  # You will need to create an insult.txt file with each insult on a new line
