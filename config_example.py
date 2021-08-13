294bb0dd9c7870ca01ad19fdfaf104811281d5a3
cjstein <cjstein9@gmail.com>
2021-08-13 10:05:18 -0500

    cleaned up example config and add comments

--- config_example.py
+++ config_example.py
@@ -1,9 +1,10 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-NICK = "subbie_bot"
-PASS = "oauth:place oauth code here"
+NICK = "bot_name"  # The name of your twitch account for the bot, all lowercase
+PASS = "oauth:place oauth code here"  # Leave the 'oauth:' part
 CHAN = "#channel_name"  # All lowercase
 RATE = (20/30)
-BAN_PAT = [
-]
-INSULT_FILE = r'absolute location of insults text file'  # You will need to create an insult.txt file with each insult on a new line
+BAN_PAT = []  # Ban patterns (currently not working correctly.  This looks for regex patterns
+
+# You will need to create an insult.txt file with each insult on a new line
+INSULT_FILE = r'absolute location of insults text file'
