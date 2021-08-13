d2d4dc279f50a916b3773d3cb3c931bffc70000c
cjstein <cjstein9@gmail.com>
2021-08-13 09:51:39 -0500

    added config example file

---
+++ config_example.py
@@ -0,0 +1,9 @@
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "subbie_bot"
+PASS = "oauth:place oauth code here"
+CHAN = "#taw_tv"
+RATE = (20/30)
+BAN_PAT = [
+]
+INSULT_FILE = r'/home/SubZeb/twitchbot/insults.txt' # you will need to create an insult.txt file with each insult on a new line
